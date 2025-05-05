a867f9ab5b16c0bbc029d772f3a044bea23d101c
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-05 12:37:44 -0600

    bug: solution bug adding the decorator to the beans

--- src/main/java/com/task/minitweet/configurations/EncoderConfiguration.java
+++ src/main/java/com/task/minitweet/configurations/EncoderConfiguration.java
@@ -1,2 +1,16 @@
-package com.task.minitweet.configurations;public class EncoderConfiguration {
+package com.task.minitweet.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class EncoderConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
