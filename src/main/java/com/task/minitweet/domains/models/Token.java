73437c8a2e1a7b71d95b34ef44716727e0cf4307
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-05 12:04:01 -0600

    add: new constructor for create token :sparkles:

--- src/main/java/com/task/minitweet/domains/models/Token.java
+++ src/main/java/com/task/minitweet/domains/models/Token.java
@@ -3,12 +3,15 @@ package com.task.minitweet.domains.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.Instant;
 import java.util.Date;
 import java.util.UUID;
 
 @Data
 @Entity
+@NoArgsConstructor
 @Table(name = "tokens")
 public class Token {
 
@@ -24,5 +27,12 @@ public class Token {
     @ManyToOne(fetch = FetchType.EAGER)
     @JsonIgnore
     private User user;
+
+    public Token(String content, User user){
+        this.content = content;
+        this.user = user;
+        this.timestamp = Date.from(Instant.now());
+        this.active = true;
+    }
 
 }
