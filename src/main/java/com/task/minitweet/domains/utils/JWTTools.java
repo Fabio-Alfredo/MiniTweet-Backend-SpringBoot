9f25c0e108cce5ec98012fb82cd636012d75cd36
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-05 11:30:39 -0600

    add: new function for create and verify tokens :sparkles:

---
+++ src/main/java/com/task/minitweet/domains/utils/JWTTools.java
@@ -0,0 +1,58 @@
+package com.task.minitweet.domains.utils;
+
+import com.task.minitweet.domains.models.User;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@Component
+public class JWTTools {
+
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private Integer expiration;
+
+    public String generateToken(User user){
+        HashMap<String, Object>claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(user.getEmail())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration * 1000L))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public Boolean verifyToken(String token){
+        try{
+            JwtParser parser = Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build();
+            parser.parse(token);
+
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public String getEmailByFrom(String token){
+        try{
+            JwtParser parser = Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build();
+            return parser.parseSignedClaims(token)
+                    .getPayload()
+                    .getSubject();
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
