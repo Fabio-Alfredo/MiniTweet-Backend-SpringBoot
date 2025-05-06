a2edbcef42b3f07dd7c8cca432b1406f72b03c1d
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-05 18:18:38 -0600

    add: new dto for login user :sparkles:

--- src/main/java/com/task/minitweet/domains/dtos/auth/LoginUserDto.java
+++ src/main/java/com/task/minitweet/domains/dtos/auth/LoginUserDto.java
@@ -1,2 +1,14 @@
-package com.task.minitweet.domains.dtos.auth;public class LoginUserDto {
+package com.task.minitweet.domains.dtos.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class LoginUserDto {
+
+    @NotBlank(message = "Identifier is required")
+    private String identifier;
+    @NotBlank(message = "Password is required")
+    private String password;
 }
