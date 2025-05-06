6fe69cad4eb5e62a0341ba4329135a77b67731ee
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-05 19:12:21 -0600

    add: new repository for functions by roles :sparkles:

---
+++ src/main/java/com/task/minitweet/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.task.minitweet.repositories;
+
+import com.task.minitweet.domains.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, String> {
+}
