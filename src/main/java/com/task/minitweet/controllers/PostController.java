cd4712c6d169672b0405e6329124c83d5c9bfa1d
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-06 11:53:18 -0600

    add: New validation to determine if the user is the owner of the posts.

--- src/main/java/com/task/minitweet/controllers/PostController.java
+++ src/main/java/com/task/minitweet/controllers/PostController.java
@@ -1,2 +1,53 @@
-package com.task.minitweet.controllers;public class PostController {
+package com.task.minitweet.controllers;
+
+import com.task.minitweet.domains.dtos.GeneralResponse;
+import com.task.minitweet.domains.dtos.post.CreatePostDto;
+import com.task.minitweet.domains.models.Post;
+import com.task.minitweet.domains.models.User;
+import com.task.minitweet.exceptions.HttpError;
+import com.task.minitweet.services.contract.PostService;
+import com.task.minitweet.services.contract.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private final PostService postService;
+    private final UserService userService;
+
+    public PostController(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<GeneralResponse>createNewPost(@ModelAttribute @Valid CreatePostDto postDto){
+        try{
+            Post post = postService.createPost(postDto);
+            return GeneralResponse.getResponse(HttpStatus.CREATED, "Post created successfully", post);
+        }catch (HttpError e){
+            return GeneralResponse.getResponse(e.getHttpStatus(), e.getMessage());
+        }
+    }
+
+    @GetMapping("/{userId}/all")
+    public ResponseEntity<GeneralResponse>findAllPostsByUser(@PathVariable String userId){
+        try{
+            User user = userService.findUserAuthenticated();
+            Boolean isOwner = user!= null && user.getId().toString().equals(userId);
+
+            List<Post>posts = postService.findAllPostsByUser(user, isOwner);
+
+            return GeneralResponse.getResponse(HttpStatus.OK, "success", posts);
+        }catch (HttpError e){
+            return GeneralResponse.getResponse(e.getHttpStatus(), e.getMessage());
+        }
+    }
+
 }
