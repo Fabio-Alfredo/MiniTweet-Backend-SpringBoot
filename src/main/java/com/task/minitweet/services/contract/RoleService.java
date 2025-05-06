e69dd0bb0a0fc5cd9706ce51262acb0c42cc70a8
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-05 19:45:38 -0600

    add: new service for create one role :sparkles:

--- src/main/java/com/task/minitweet/services/contract/RoleService.java
+++ src/main/java/com/task/minitweet/services/contract/RoleService.java
@@ -1,6 +1,7 @@
 package com.task.minitweet.services.contract;
 
 
+import com.task.minitweet.domains.dtos.role.CreateRoleDto;
 import com.task.minitweet.domains.models.Role;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface RoleService {
     Role findRoleById(String id);
     List<Role> findAllRoles();
+    void createRole(CreateRoleDto roleDto);
 }
--- src/main/java/com/task/minitweet/services/impl/RoleServiceImpl.java
+++ src/main/java/com/task/minitweet/services/impl/RoleServiceImpl.java
@@ -1,9 +1,11 @@
 package com.task.minitweet.services.impl;
 
+import com.task.minitweet.domains.dtos.role.CreateRoleDto;
 import com.task.minitweet.domains.models.Role;
 import com.task.minitweet.exceptions.HttpError;
 import com.task.minitweet.repositories.RoleRepository;
 import com.task.minitweet.services.contract.RoleService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,11 @@ import java.util.List;
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final ModelMapper modelMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository, ModelMapper modelMapper) {
         this.roleRepository = roleRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -40,6 +44,21 @@ public class RoleServiceImpl implements RoleService {
                 throw new HttpError(HttpStatus.NOT_FOUND, "No roles found");
             }
             return roles;
+        }catch (HttpError e ){
+            throw e;
+        }
+    }
+
+    @Override
+    public void createRole(CreateRoleDto roleDto) {
+        try{
+            Role role = roleRepository.findById(roleDto.getId()).orElse(null);
+            if(role != null){
+                throw new HttpError(HttpStatus.CONFLICT, "Role already exists");
+            }
+            role = modelMapper.map(roleDto, Role.class);
+
+            roleRepository.save(role);
         }catch (HttpError e ){
             throw e;
         }
