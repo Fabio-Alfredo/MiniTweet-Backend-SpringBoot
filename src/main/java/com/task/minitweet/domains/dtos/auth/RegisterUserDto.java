ab9e93ecd9661a7fb97b6a08b0616525c61452a4
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-05 18:13:49 -0600

    add: new dto for register user :sparkles:

--- src/main/java/com/task/minitweet/domains/dtos/auth/RegisterUserDto.java
+++ src/main/java/com/task/minitweet/domains/dtos/auth/RegisterUserDto.java
@@ -1,2 +1,17 @@
-package com.task.minitweet.domains.dtos.auth;public class RegisterUserDto {
+package com.task.minitweet.domains.dtos.auth;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class RegisterUserDto {
+
+    @NotEmpty(message = "Username is required")
+    private String username;
+    @NotEmpty(message = "Password is required")
+    private String email;
+    @NotEmpty(message = "Password is required")
+    private String password;
+    private String biography;
+    private String profilePicture;
 }
