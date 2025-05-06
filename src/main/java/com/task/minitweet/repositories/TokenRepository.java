865b2fe906878e96f1fbd6afe6ed44dec797396c
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-05 21:20:11 -0600

    add: new decorator for repositories :sparkles:

--- src/main/java/com/task/minitweet/repositories/TokenRepository.java
+++ src/main/java/com/task/minitweet/repositories/TokenRepository.java
@@ -3,10 +3,12 @@ package com.task.minitweet.repositories;
 import com.task.minitweet.domains.models.Token;
 import com.task.minitweet.domains.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, UUID> {
     List<Token>findByUserAndActive(User user, Boolean active);
 }
--- src/main/java/com/task/minitweet/repositories/UserRepository.java
+++ src/main/java/com/task/minitweet/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.task.minitweet.repositories;
 
 import com.task.minitweet.domains.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     User findByUsernameOrEmail(String username, String email);
     User findByEmail(String email);
