083db99fc693a9f7c98e769b8d18f93a0eacccfd
Fabio-Alfredo <00210121@uca.edu.sv>
2025-05-06 11:52:27 -0600

    add: new function for find posts by user :sparkles:

--- src/main/java/com/task/minitweet/repositories/PostRepository.java
+++ src/main/java/com/task/minitweet/repositories/PostRepository.java
@@ -1,2 +1,14 @@
-package com.task.minitweet.repositories;public class PostRepository {
+package com.task.minitweet.repositories;
+
+import com.task.minitweet.domains.models.Post;
+import com.task.minitweet.domains.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+    List<Post>findAllByAuthor(User author);
 }
